9944d3aab77b3a9cbc2f70c019b6edc2ed13c94c
Artem Dvinin <dvinin.artem@gmail.com>
2016-03-21 19:03:42 +0300

    Add empty class UserMealServiceTest

---
+++ src/test/java/ru/javawebinar/topjava/service/UserMealServiceTest.java
@@ -0,0 +1,56 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class UserMealServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBetweenDates() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBetweenDateTimes() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testSave() throws Exception {
+
+    }
+}
