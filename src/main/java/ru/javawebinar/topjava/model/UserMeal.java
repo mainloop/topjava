3821edec188dbc69f6780efa6a9a400f12b19b53
Artem Dvinin <dvinin.artem@gmail.com>
2016-03-29 18:09:47 +0300

    UserMealServiceTest works fine, but conversion from LocalDateTime to Timestamp is still needed.

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -1,22 +1,44 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 /**
  * GKislin
  * 11.01.2015.
  */
+
+@NamedQueries({
+        @NamedQuery(name = UserMeal.GET, query = "SELECT um FROM UserMeal um WHERE um.id=:id AND um.user.id=:user_id"),
+        @NamedQuery(name = UserMeal.DELETE, query = "DELETE FROM UserMeal um WHERE um.id=:id AND um.user.id=:user_id"),
+        @NamedQuery(name = UserMeal.ALL_SORTED, query = "SELECT um FROM UserMeal um WHERE um.user.id=:user_id ORDER BY um.dateTime DESC"),
+        @NamedQuery(name = UserMeal.BY_DATE_TIME, query = "SELECT um FROM UserMeal um WHERE um.user.id=:user_id AND um.dateTime BETWEEN :start AND :stop ORDER BY um.dateTime DESC"),
+        @NamedQuery(name = UserMeal.UPDATE, query = "UPDATE UserMeal um SET um.description=:description, um.calories=:calories, um.dateTime=:date_time  WHERE um.id=:id AND um.user.id=:user_id"),
+})
+@Entity
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id"}, name = "meals_unique_user_datetime_idx")})
 public class UserMeal extends BaseEntity {
 
-    private LocalDateTime dateTime;
+    public static final String UPDATE = "Meal.update";
+    public static final String GET = "Meal.get";
+    public static final String DELETE = "Meal.delete";
+    public static final String ALL_SORTED = "Meal.getAllSorted";
+    public static final String BY_DATE_TIME = "Meal.getByDateTime";
 
+
+    @Column(name = "date_time")
+    private Timestamp dateTime;
+    // private LocalDateTime dateTime;
+
+    @Column(name = "description")
     private String description;
 
+    @Column(name = "calories", nullable = false)
     protected int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", unique = true, nullable = false)
     private User user;
 
     public UserMeal() {
@@ -28,13 +50,22 @@ public class UserMeal extends BaseEntity {
 
     public UserMeal(Integer id, LocalDateTime dateTime, String description, int calories) {
         super(id);
-        this.dateTime = dateTime;
+        this.dateTime = Timestamp.valueOf(dateTime);
         this.description = description;
         this.calories = calories;
     }
 
+    public UserMeal(User user, Integer id, LocalDateTime dateTime, String description, int calories) {
+        super(id);
+        this.dateTime = Timestamp.valueOf(dateTime);
+        this.description = description;
+        this.calories = calories;
+        this.user = user;
+    }
+
+
     public LocalDateTime getDateTime() {
-        return dateTime;
+        return dateTime.toLocalDateTime();
     }
 
     public String getDescription() {
@@ -46,7 +77,7 @@ public class UserMeal extends BaseEntity {
     }
 
     public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
+        this.dateTime = Timestamp.valueOf(dateTime);
     }
 
     public void setDescription(String description) {
--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaUserMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaUserMealRepositoryImpl.java
@@ -1,9 +1,14 @@
 package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserMealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -13,30 +18,63 @@ import java.util.List;
  */
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaUserMealRepositoryImpl implements UserMealRepository {
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
+    @Transactional
     public UserMeal save(UserMeal userMeal, int userId) {
-        return null;
+        if (userMeal.isNew()) {
+            em.persist(userMeal);
+            return userMeal;
+        } else {
+            if (em.createNamedQuery(UserMeal.UPDATE)
+                    .setParameter("description", userMeal.getDescription())
+                    .setParameter("date_time", Timestamp.valueOf(userMeal.getDateTime()))
+                    .setParameter("calories", userMeal.getCalories())
+                    .setParameter("id", userMeal.getId())
+                    .setParameter("user_id", userId).executeUpdate() == 0) {
+                return null;
+            }
+            return userMeal;
+        }
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        return em.createNamedQuery(UserMeal.DELETE)
+                .setParameter("id", id)
+                .setParameter("user_id", userId)
+                .executeUpdate() != 0;
     }
 
     @Override
     public UserMeal get(int id, int userId) {
-        return null;
+        try {
+            return em.createNamedQuery(UserMeal.GET, UserMeal.class)
+                    .setParameter("id", id)
+                    .setParameter("user_id", userId)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 
     @Override
     public List<UserMeal> getAll(int userId) {
-        return null;
+        return em.createNamedQuery(UserMeal.ALL_SORTED, UserMeal.class).setParameter("user_id", userId).getResultList();
     }
 
     @Override
     public List<UserMeal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return em.createNamedQuery(UserMeal.BY_DATE_TIME, UserMeal.class)
+                .setParameter("user_id", userId)
+                .setParameter("start", Timestamp.valueOf(startDate))
+                .setParameter("stop", Timestamp.valueOf(endDate))
+                .getResultList();
     }
 }
